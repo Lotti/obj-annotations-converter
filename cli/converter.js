a40087355cb8bd955740e3c702d42ef469998f5d
Valerio Riva <valerio.riva@it.ibm.com>
2019-07-31 09:59:34 +0200

    from voc to watson: implemented

--- cli/converter.js
+++ cli/converter.js
@@ -2,13 +2,12 @@
 
 require(`dotenv`).config();
 const path = require(`path`);
-const log4js = require(`log4js`);
-log4js.configure(path.join(__dirname, `..`, `log4js.json`));
 const program = require(`commander`);
-const log = log4js.getLogger(`converter`);
+const chalk = require('chalk');
 const fg = require(`fast-glob`);
 const fs = require(`fs-extra`);
 const xmlbuilder = require(`xmlbuilder`);
+const xmlParser = require('fast-xml-parser');
 
 program
     .version(`0.1`)
@@ -20,7 +19,7 @@ program
     .parse(process.argv);
 
 if (program.from === program.to) {
-    log.fatal(`Can't proceed with same format as origin and destination`);
+    console.error(chalk.bold.red(`Can't proceed with same format as origin and destination`));
     process.exit(1);
 }
 
@@ -36,21 +35,21 @@ if (program.target) {
 
 try {
     if (!fs.lstatSync(source).isDirectory()) {
-        log.fatal(`Path source ${source} must be a directory`);
+        console.error(chalk.bold.red(`Path source ${source} must be a directory`));
         process.exit(1);
     }
 } catch (error) {
-    log.fatal(`Path source ${source} doesn't exists!`);
+    console.error(chalk.bold.red(`Path source ${source} doesn't exists!`));
     process.exit(1);
 }
 
 try {
     if (!fs.lstatSync(target).isDirectory()) {
-        log.fatal(`Path target ${target} must be a directory`);
+        console.error(chalk.bold.red(`Path target ${target} must be a directory`));
         process.exit(1);
     }
 } catch (error) {
-    log.warn(`Path target ${target} doesn't exists, creating it...`);
+    console.warn(chalk.yellowBright(`Path target ${target} doesn't exists, creating it...`));
     fs.ensureDirSync(target);
 }
 
@@ -58,38 +57,38 @@ const options = {onlyFiles: true, deep: 0, absolute: true};
 
 if (program.from === `watson` && program.to === `voc`) {
     const entries = fg.sync(path.join(source, `*.json`), options);
-    log.info(`Found ${entries.length} entries.`);
+    console.log(chalk.green(`Found ${entries.length} entries.`));
     let i = 0;
     for (const entry of entries) {
         fs.readFile(entry, {encoding: `utf8`}, (err, data) => {
             if (err) {
-                log.error(`Can't access file ${entry}. Skipping it.`);
+                console.error(chalk.red(`Can't access file ${entry}. Skipping it.`));
             } else {
                 try {
                     const json = JSON.parse(data);
 
                     const xml = xmlbuilder.create(`annotation`);
-                    xml.ele(`folder`, {}, 'Unspecified');
+                    xml.ele(`folder`, {}, `Unspecified`);
                     xml.ele(`filename`, {}, json.source.filename);
                     xml.ele(`path`, {}, path.join(source, json.source.filename));
                     xml.ele(`source`).ele(`database`, {}, json.image_id);
                     const size = xml.ele(`size`);
-                    size.ele('width', {}, json.dimensions.width);
-                    size.ele('height', {}, json.dimensions.height);
-                    size.ele('dept', {}, 3);
+                    size.ele(`width`, {}, json.dimensions.width);
+                    size.ele(`height`, {}, json.dimensions.height);
+                    size.ele(`dept`, {}, 3);
                     xml.ele(`segmented`, {}, 0);
 
                     for (const o of json.training_data.objects) {
                         const obj = xml.ele(`object`);
-                        obj.ele('name', {}, o.object);
-                        obj.ele('pose', {}, 'Unspecified');
-                        obj.ele('truncated', {}, 0);
-                        obj.ele('difficult', {}, 0);
-                        const bndbox = obj.ele('bndbox');
-                        bndbox.ele('xmin', {}, o.location.left);
-                        bndbox.ele('ymin', {}, o.location.top);
-                        bndbox.ele('xmax', {}, o.location.left + o.location.width);
-                        bndbox.ele('ymax', {}, o.location.top + o.location.height);
+                        obj.ele(`name`, {}, o.object);
+                        obj.ele(`pose`, {}, `Unspecified`);
+                        obj.ele(`truncated`, {}, 0);
+                        obj.ele(`difficult`, {}, 0);
+                        const bndbox = obj.ele(`bndbox`);
+                        bndbox.ele(`xmin`, {}, o.location.left);
+                        bndbox.ele(`ymin`, {}, o.location.top);
+                        bndbox.ele(`xmax`, {}, o.location.left + o.location.width);
+                        bndbox.ele(`ymax`, {}, o.location.top + o.location.height);
                     }
 
                     const fileName = `${path.basename(entry, `.json`)}.xml`;
@@ -97,25 +96,91 @@ if (program.from === `watson` && program.to === `voc`) {
                     const xmlString = xml.end({pretty: true});
                     fs.writeFile(fileDst, xmlString, {encoding: `utf8`}, (err) => {
                         if (err) {
-                            log.error(`Can't write file ${fileDst}. Skipping it.`);
+                            console.error(chalk.red(`Can't write file ${fileDst}. Skipping it.`));
                         } else {
                             i++;
-                            log.info(`${fileDst} generated`);
+                            console.log(`${fileDst} generated`);
                         }
                     });
                 } catch (error) {
-                    log.error(`Can't parse file ${entry}. Skipping it.`);
-                    log.error(error);
+                    console.error(chalk.red(`Can't parse file ${entry}. Skipping it.`));
                 }
             }
         });
     }
 } else if (program.from === `voc` && program.to === `watson`) {
     const entries = fg.sync(path.join(program.source, `*.xml`), options);
+    console.log(chalk.green(`Found ${entries.length} entries.`));
+    let i = 0;
     for (const entry of entries) {
-        log.info(entry);
+        fs.readFile(entry, {encoding: `utf8`}, (err, xmlData) => {
+            if (err) {
+                console.error(chalk.red(`Can't access file ${entry}. Skipping it.`));
+            } else {
+                var options = {
+                    attributeNamePrefix: "@_",
+                    attrNodeName: "attr", //default is 'false'
+                    textNodeName: "#text",
+                    ignoreAttributes: true,
+                    ignoreNameSpace: false,
+                    allowBooleanAttributes: false,
+                    parseNodeValue: true,
+                    parseAttributeValue: false,
+                    trimValues: true,
+                    cdataTagName: "__cdata", //default is 'false'
+                    cdataPositionChar: "\\c",
+                    localeRange: "", //To support non english character in tag/attribute values.
+                    parseTrueNumberOnly: false,
+                };
+
+                if (xmlParser.validate(xmlData) !== true) {
+                    console.error(chalk.red(`Can't parse file ${entry}. Skipping it.`));
+                    return;
+                }
+                const jsonObj = xmlParser.convertToJson(xmlParser.getTraversalObj(xmlData, options), options);
+                jsonObj.annotation.object = Array.isArray(jsonObj.annotation.object) ? jsonObj.annotation.object : [jsonObj.annotation.object];
+                const json = {
+                    "updated": new Date().toISOString(),
+                    "dimensions": {
+                        "width": jsonObj.annotation.size.width,
+                        "height": jsonObj.annotation.size.height
+                    },
+                    "source": {
+                        "type": "file",
+                        "filename": jsonObj.annotation.filename
+                    },
+                    "created": new Date().toISOString(),
+                    "image_id": jsonObj.annotation.source.database,
+                    "training_data": {
+                        "objects": jsonObj.annotation.object.map((o) => {
+                            return {
+                                object: o.name,
+                                location: {
+                                    width: o.bndbox.ymax - o.bndbox.ymin,
+                                    top: o.bndbox.ymin,
+                                    height: o.bndbox.xmax - o.bndbox.xmin,
+                                    left: o.bndbox.xmin
+                                }
+                            };
+                        })
+                    }
+                };
+
+                const fileName = `${path.basename(entry, `.xml`)}.json`;
+                const fileDst = path.join(target, fileName);
+                const jsonString = JSON.stringify(json, null, 4);
+                fs.writeFile(fileDst, jsonString, {encoding: `utf8`}, (err) => {
+                    if (err) {
+                        console.error(chalk.red(`Can't write file ${fileDst}. Skipping it.`));
+                    } else {
+                        i++;
+                        console.log(`${fileDst} generated`);
+                    }
+                });
+            }
+        });
     }
 } else {
-    log.fatal(`Can't convert from ${program.from} to ${program.to}: case not supported!`);
+    console.error(chalk.bold.red(`Can't convert from ${program.from} to ${program.to}: case not supported!`));
     process.exit(1);
 }
